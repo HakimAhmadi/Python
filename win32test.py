91a16cb16909bfa83e03ac349e6d481d92cb4b04
HakimAhmadi <hakimsshah@gmail.com>
2022-12-21 16:15:54 +0500

    File cleaned

--- win32test.py
+++ win32test.py
@@ -1,57 +1,7 @@
-import win32file
-import win32inet
-import win32profile
-import win32ui
-import win32wnet as wnet    
-import win32api as beep
-import win32api
-import win32con
+# the module to work with outlook
 import win32com.client
 import datetime
 
-# file = win32file.FindFilesW('C:\\Users\\abdul\\Desktop\\*', Transaction=None)
-# win32file.DecryptFile("C:\\Users\\abdul\\Desktop\\new.txt")
-# print(file)
-# for i in file:
-#     print(i[-1])
-# for i in range(10):
-#     beep.Beep(500*i,500)
-# print(win32profile.GetAllUsersProfileDirectory())
-# print(win32ui.Is())
-# string = wnet.WNetGetConnection()
-# print(string)
-
-# Reading cursor position
-# while True:
-#     pos = win32api.GetCursorPos()
-#     print(pos)
-
-# set cursor position
-# pos = (200, 200)
-# win32api.SetCursorPos(pos)
-
-
-# def mouse_click(x, y):
-#     win32api.SetCursorPos((x, y))
-#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-#     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-
-# while True:
-#     mouse_click(20, 60)
-
-
-  
-# while 1:
-#     print("Enter the word you want to speak it out by computer")
-#     s = input()
-#     speaker.Speak(s)
-  
-
-
-
-
-
-# the module to work with outlook
 
 def speak(str):
     speaker = win32com.client.Dispatch("SAPI.SpVoice")
@@ -105,14 +55,6 @@ def reading_outlook_email():
                 )
         
             emails.append(this_message)
-        
-    #collecting all the emails that is of todays date on system
-        # if this_message[4] == datetime.date.today():
-        #     emails.append(this_message)
-            
-        # else:
-        #     break
-    
 
     speak(f"there are {len(emails)} emails for today" )
     
